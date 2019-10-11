ef25b15019f843e6a34d0729467755c7ad3433ff
shteniovych <victor.shteniovych@gmail.com>
2019-10-11 18:57:59 +0300

    Added new folders and files

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
 /*
     Here should be implemented sign in logic
 */
+
+//var expectedUserLogin = 'npi'
+//var expectedUserPassword = '123456'
+//
+////get user's date
+//var userName = prompt("Enter your login");
+//var userPassword = prompt("Enter your password");
+//
+///*
+//Check if user's entered date are equal to expected
+//*/
+//if(expectedUserLogin == userName && expectedUserPassword == userPassword) {
+//    alert("Welcome to our web site!!!")
+//}
+//else {
+//    //remove content element and set body's background
+//    var e = document.getElementById("page");
+//    e.parentNode.removeChild(e);
+//    document.body.style.backgroundImage="url('images/403.jpg')";
+//}
